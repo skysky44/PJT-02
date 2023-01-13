c6826bdc0ad50c80a00682844712ac083475d080
hphk-beemo <beemo@hphk.kr>
2023-01-13 10:11:06 +0900

    Update 04.py"

--- 04.py
+++ 04.py
@@ -10,7 +10,7 @@ def search_movie(title):
 # 아래의 코드는 수정하지 않습니다.
 if __name__ == '__main__':
     """
-    영화 제목으로 영화 검색하여 영화 id 출력
+    제목에 해당하는 영화가 있으면 해당 영화의 id 반환
     검색한 결과 영화가 없다면 None을 반환
     """
     print(search_movie('기생충'))
